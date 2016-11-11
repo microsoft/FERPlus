f1a8cb22f992996c67e7d00eb03580e5b5675097
Emad Barsoum <ebarsoum@microsoft.com>
2016-11-10 18:21:34 -0800

    Add license banner and the training data layout structure.

--- src/img_util.py
+++ src/img_util.py
@@ -1,4 +1,9 @@
-﻿import numpy as np
+﻿#
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+#
+
+import numpy as np
 import random as rnd
 from PIL import Image
 from scipy import ndimage
